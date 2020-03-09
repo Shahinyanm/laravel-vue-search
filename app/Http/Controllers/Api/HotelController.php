76e88eec3dd0bca8007763f1a6fd69b38daed9bc
shahinyanm <shahinyanm@gmail.com>
2020-03-09 20:01:27 +0400

    add HotelController, index() and search() methods

--- app/Http/Controllers/Api/HotelController.php
+++ app/Http/Controllers/Api/HotelController.php
@@ -8,13 +8,54 @@ use App\Models\Hotel;
 
 class HotelController extends Controller
 {
-    public function index($query = null): Hotels
+    public function index(): Hotels
     {
-        $hotels = new Hotel();
-        if($query){
+        return new Hotels(Hotel::all());
+    }
+    
+    public function search(Request $request)
+    {
+        $term = trim($request->query);
+        if (empty($term)) {
+            return response()->json([]);
+        }
+        $hotels = Hotel::query();
         
+        if (!$request->query) {
+            return response(['status' => 'failed'], 403);
         }
         
-        return new Hotels();
+        if ($request->has('name')) {
+            $query = trim($request->name);
+            $hotels = $hotels->where('name', 'LIKE', '%'.$query.'%');
+        }
+        
+        if ($request->has('bedrooms')) {
+            $hotels = $hotels->where('bedrooms', (int) $request->bedrooms);
+        }
+        
+        if ($request->has('bathrooms')) {
+            $hotels = $hotels->where('bathrooms', (int) $request->bathrooms);
+        }
+        
+        if ($request->has('storeys')) {
+            $hotels = $hotels->where('storeys', (int) $request->storeys);
+        }
+        
+        if ($request->has('garages')) {
+            $hotels = $hotels->where('garages', (int) $request->garages);
+        }
+        
+        if ($request->has('start_price')) {
+            $hotels = $hotels->where('price', '>=', $request->start_price);
+        }
+        
+        if ($request->has('end_price')) {
+            $hotels = $hotels->where('price', '>=', $request->end_price);
+        }
+                
+        
+        // This should do automatic and nice JSON
+        return new Hotels($hotels->get());
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,4 +12,5 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('/{query}','Api\HotelController@index');
+Route::get('/', 'Api\HotelController@index');
+Route::get('search', 'Api\HotelController@search');
